a5b4a8d88abaa4ecef44caa9f2beaabd6ad5c37a
comunet_dcoyle <dcoyle@comunet.com.au>
2023-03-21 13:18:25 +1030

    new webpack.mix.js

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,9 +1,18 @@
-const { mix } = require('laravel-mix');
+const mix = require('laravel-mix');
+const path = require('path');
 
-mix.js('resources/assets/js/app.js','public/js')
-  .sass('resources/assets/sass/app.scss', 'public/css');
-// mix.js('resources/assets/js/app.js','public/js').vue();
-// mix.sass('resources/assets/sass/app.scss', 'public/css');
-  
-/* Optional: uncomment for bootstrap fonts */
-// mix.copy('node_modules/bootstrap-sass/assets/fonts/bootstrap/','public/fonts/bootstrap');
+mix.js('resources/assets/js/app.js', 'public/js')
+  .sass('resources/assets/sass/app.scss', 'public/css')
+  .webpackConfig({
+    output: {
+      chunkFilename: 'js/[name].js?id=[chunkhash]',
+    },
+    resolve: {
+      alias: {
+        '@': path.resolve('resources/assets/js'),
+      },
+    },
+  })
+  .vue()
+  .version()
+  .sourceMaps();
