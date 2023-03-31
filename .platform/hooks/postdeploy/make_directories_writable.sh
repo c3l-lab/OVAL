4d0bf9f5e282aa76f82b252506c6486a48855efa
comunet_dcoyle <dcoyle@comunet.com.au>
2023-03-31 16:10:06 +1030

    Added logging.php added in laravel 5.6

--- .platform/hooks/postdeploy/make_directories_writable.sh
+++ .platform/hooks/postdeploy/make_directories_writable.sh
@@ -2,5 +2,6 @@
 
 # Laravel requires some directories to be writable.
 
-sudo chmod -R 755 storage/
-sudo chmod -R 755 bootstrap/cache/
+sudo chmod -R 755 /var/app/current/storage
+sudo chmod -R 755 /var/app/current/bootstrap 
+sudo chmod -R 755 /var/app/current/bootstrap/cache
