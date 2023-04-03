a74b03c36eeeaaf84e9bb8bfedff482da46c3bb5
Daniel Ebbert <daniel@ebbert.nrw>
2023-04-03 15:37:08 +0930

    Add clearing of the php cache before new creation

--- .platform/hooks/postdeploy/optimize_laravel.sh
+++ .platform/hooks/postdeploy/optimize_laravel.sh
@@ -3,8 +3,11 @@
 # Optimizing configuration loading, route loading and view loading
 # https://laravel.com/docs/8.x/deployment#optimization
 
+php artisan config:clear
 php artisan config:cache
 
+php artisan route:clear
 php artisan route:cache
 
+php artisan view:clear
 php artisan view:cache
