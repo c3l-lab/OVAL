e648f3f432912c61282f5ff896cf6298b1c131de
Wei Zhu <yesmeck@gmail.com>
2023-06-21 11:24:09 +0930

    mark all fields requied

--- resources/views/lti/registrations/_form.blade.php
+++ resources/views/lti/registrations/_form.blade.php
@@ -21,43 +21,43 @@
                     <div class="form-group">
                         <label for="registration-name">Name</label>
                         <input type="text" id="lti-registration-name" class="form-control gray-textbox"
-                            name="name" value="{{ isset($registration) ? $registration->name : "" }}">
+                            name="name" required value="{{ isset($registration) ? $registration->name : "" }}">
                     </div><!-- form-group -->
 
                     <div class="form-group">
                         <label for="registration-issuer">Issuer</label>
                         <input type="text" id="lti-registration-issuer" class="form-control gray-textbox"
-                            name="issuer" placeholder="https://lift.c3l.ai" value="{{ isset($registration) ? $registration->issuer : "" }}">
+                            name="issuer" required placeholder="https://lift.c3l.ai" value="{{ isset($registration) ? $registration->issuer : "" }}">
                     </div><!-- form-group -->
 
                           <div class="form-group">
                         <label for="registration-client-id">Client ID</label>
                         <input type="text" id="registration-client-id" class="form-control gray-textbox"
-                            name="client_id" value="{{ isset($registration) ? $registration->client_id : "" }}">
+                            name="client_id" required value="{{ isset($registration) ? $registration->client_id : "" }}">
                     </div><!-- form-group -->
 
                     <div class="form-group">
                         <label for="registration-registration-id">Deployment ID</label>
                         <input type="text" id="registration-registration-id" class="form-control gray-textbox"
-                            name="deployment_id" value="{{ isset($registration) ? $registration->deployment_id : "" }}">
+                            name="deployment_id" required value="{{ isset($registration) ? $registration->deployment_id : "" }}">
                     </div><!-- form-group -->
 
                     <div class="form-group">
                         <label for="registration-keyset-url">Keyset URL</label>
                         <input type="text" id="registration-keyset-url" class="form-control gray-textbox"
-                            name="keyset_url" value="{{ isset($registration) ? $registration->keyset_url : "" }}">
+                            name="keyset_url" required value="{{ isset($registration) ? $registration->keyset_url : "" }}">
                     </div><!-- form-group -->
 
                     <div class="form-group">
                         <label for="registration-auth-token-url">Access Token URL</label>
                         <input type="text" id="registration-auth-token-url" class="form-control gray-textbox"
-                            name="auth_token_url" value="{{ isset($registration) ? $registration->auth_token_url : "" }}">
+                            name="auth_token_url" required value="{{ isset($registration) ? $registration->auth_token_url : "" }}">
                     </div><!-- form-group -->
 
                     <div class="form-group">
                         <label for="registration-auth-login-url">Login URL</label>
                         <input type="text" id="registration-auth-login-url" class="form-control gray-textbox"
-                            name="auth_login_url" value="{{ isset($registration) ? $registration->auth_login_url : "" }}">
+                            name="auth_login_url" required value="{{ isset($registration) ? $registration->auth_login_url : "" }}">
                     </div><!-- form-group -->
                 </div><!-- col -->
             </div><!-- row -->
