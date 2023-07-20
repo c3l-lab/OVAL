c4955153bb9d4f1571a8cda9c585ae90ed7ed8a4
Wei Zhu <yesmeck@gmail.com>
2023-07-20 22:29:45 +0930

    remove axios

--- resources/assets/js/bootstrap.js
+++ resources/assets/js/bootstrap.js
@@ -9,19 +9,6 @@ window.$ = window.jQuery = require('jquery');
 require('bootstrap-sass');
 
 /**
- * We'll register a HTTP interceptor to attach the "CSRF" header to each of
- * the outgoing requests issued by this application. The CSRF middleware
- * included with Laravel will automatically verify the header's value.
- */
-
-window.axios = require('axios');
-
-window.axios.defaults.headers.common = {
-    'X-CSRF-TOKEN': window.Laravel.csrfToken,
-    'X-Requested-With': 'XMLHttpRequest'
-};
-
-/**
  * Echo exposes an expressive API for subscribing to channels and listening
  * for events that are broadcast by Laravel. Echo and event broadcasting
  * allows your team to easily build robust real-time web applications.
