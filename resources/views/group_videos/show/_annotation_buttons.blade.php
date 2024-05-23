ca7cf323d02f8b09dfa8b7d3639d82f82cea55de
Minh Truong <minhtruongdevvn@gmail.com>
2024-05-23 20:03:09 +0930

    feat(annotation): allow display config for annotation
    https://github.com/c3l-lab/OVAL/issues/12
    
    https://github.com/c3l-lab/OVAL/issues/13

--- resources/views/group_videos/show/_annotation_buttons.blade.php
+++ resources/views/group_videos/show/_annotation_buttons.blade.php
@@ -1,5 +1,5 @@
 <div class="annotations-buttons">
-    @if ($group_video->annotation_config['downloadable'])
+    @if (data_get($group_video->annotation_config, 'downloadable', true))
         <a href="{{ route('annotations.download', ['group_video_id' => $group_video->id, 'course_id' => $course->id]) }}"
             target="_blank">
             <button type="button" class="btn btn-link download-comments" title="Download annotations and comments">
@@ -8,9 +8,9 @@
             </button>
         </a>
     @endif
-    @if ($group_video->annotation_config['is_show_annotation_button'])
+    @if (data_get($group_video->annotation_config, 'is_show_annotation_button', true))
         <button type="button" class="btn btn-link add-annotation" title="Add an annotation">
-            {{ $group_video->annotation_config['label'] }}
+            {{ data_get($group_video->annotation_config, 'label', 'New Annotation') }}
             <i class="fa fa-plus-circle"></i>
         </button>
     @endif
