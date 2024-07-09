9bb6781f6d4435ed93d8733895b0ea446600a734
Minh Truong <minhtruongdevvn@gmail.com>
2024-07-09 09:53:34 +0930

    fix(eye_tracking)

--- resources/assets/js/eye-tracking.js
+++ resources/assets/js/eye-tracking.js
@@ -74,7 +74,7 @@ if (gazeConsent && gazeConsent[2] === "true") {
             dot.style.zIndex = 99999;
 
             const flag = document.createElement('span');
-            flag.flagContent = 'C';
+            flag.textContent = 'C';
             flag.style.position = 'absolute';
             flag.style.color = 'black';
             flag.style.zIndex = 99999;
